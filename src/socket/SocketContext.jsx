4fa45abf001511365cbabd3409033a526891b731
Padmanaban <padmanaban870@gmail.com>
2024-09-21 18:52:12 +0530

    Config changed

--- src/socket/SocketContext.jsx
+++ src/socket/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("https://chatspace-backend-b5gh.onrender.com",{
+            const socket = io("http://localhost:8000",{
                 query: {
                     userId : authUser._id
                 }
